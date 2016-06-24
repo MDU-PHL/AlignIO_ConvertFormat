7eaf9aa64d87aae94b86abc996946d112ab0b26d
Mark Schultz <mark.schultz@unimelb.edu.au>
2016-06-24 14:32:43 +1000

    added support for amino acid alignments

--- AlignIO_ConvertFormat.py
+++ AlignIO_ConvertFormat.py
@@ -11,7 +11,8 @@ import argparse
 import os
 import sys
 from Bio import AlignIO
-from Bio.Alphabet import generic_dna
+from Bio.Alphabet import generic_dna, generic_protein
+# from Bio.Alphabet import IUPAC
 
 #set up the arguments parser to deal with the command line input
 PARSER = argparse.ArgumentParser(description='Convert DNA alignments between \
@@ -21,13 +22,18 @@ PARSER.add_argument('-a', '--alignment', help='Input alignment(s)', nargs='+',
                     required=True)
 PARSER.add_argument('-i', '--informat', help='Alignment input format (default \
                     fasta)', default='fasta', required=False)
+PARSER.add_argument('-l', '--letter_alphabet', help='Use A for Amino Acids, \
+                    D for DNA', default='D', required=False)
 PARSER.add_argument('-o', '--outformat', help='Alignment output format \
                     (default fasta)', default='fasta', required=False)
 
 ARGS = PARSER.parse_args()
 
-def read_alignment(alignment, informat, outformat):
-    align = AlignIO.read(alignment, informat, alphabet=generic_dna)
+def read_alignment(alignment, letter_alphabet, informat, outformat):
+    if letter_alphabet.upper() == 'D':
+        align = AlignIO.read(alignment, informat, alphabet=generic_dna)
+    if letter_alphabet.upper() == 'A':
+        align = AlignIO.read(alignment, informat, alphabet=generic_protein)
     out_basename = os.path.splitext(alignment)[0]
     outname = out_basename+'.'+outformat
     if os.path.exists(outname):
@@ -40,4 +46,4 @@ def read_alignment(alignment, informat, outformat):
             print align
             print ''
 for i in ARGS.alignment:
-    read_alignment(i, ARGS.informat, ARGS.outformat)
+    read_alignment(i, ARGS.letter_alphabet, ARGS.informat, ARGS.outformat)
